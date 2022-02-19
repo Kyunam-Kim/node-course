15445e232e7421286473933747fc3ac028750084
Kyunam-Kim <rlarbska3824@gmail.com>
2022-02-19 22:41:24 +0900

    add

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,19 +7,36 @@ console.log("Client side javascript file is loadedf");
 // });
 
 // fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/boston.json?access_token=pk.eyJ1Ijoia3l1MTIzIiwiYSI6ImNreXFyZGw0cDBtbXMyd3AyOHBhbDdlNzgifQ.OUPRUvvwILVz0r5p7roJ_g&limit=1').then()
-fetch("http://localhost:3000/weather?address=boston").then((response) => {
-  response.json().then((data) => {
+
+// fetch("http://localhost:3000/weather?address=boston").then((response) => {
+//   response.json().then((data) => {
+//     if (data.error) {
+//       console.log(data.error);
+//     } else {
+//       console.log(data.location);
+//       console.log(data.forecast);
+//     }
+//   });
+// });
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  fetch("http://localhost:3000/weather?address="+location).then((response) => {
+    response.json().then((data) => {
       if (data.error) {
-          console.log(data.error)
+        messageOne.textContent = data.error
+        messageTwo.textContent = ''
       } else {
-          console.log(data.location)
-          console.log(data.forecast)
+          messageOne.textContent = data.location
+          messageTwo.textContent = data.forecast
       }
-    })
+    });
+  });
+  console.log(location);
 });
-
-const weatherForm = document.querySelector('form')
-
-weatherForm.addEventListener('submit', () => {
-    console.log('testing!')
-})
