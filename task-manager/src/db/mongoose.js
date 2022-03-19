5862532262b97403d90a75e07ce49b56598b1ca1
Kyunam-Kim <rlarbska3824@gmail.com>
2022-03-20 04:38:36 +0900

    src/models/task, user.js added and modified

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -5,22 +5,24 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     // useCreateIndex: true
 })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-})
+// const me = new User({
+//     name: '     Kyunam',
+//     password: '    password123',
+//     email: 'MYEMAIL@MEAD.IO'
+// })
 
-const myTask = new Task({
-    description: 'Clean your rooom',
-    completed: false
-})
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
 
-myTask.save().then(() => {
-    console.log(myTask)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// const myTask = new Task({
+//     description: '    Do flip   '
+// })
+
+// myTask.save().then(() => {
+//     console.log(myTask)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
